3baac81ec92faca4e101fb974806b4807cd51d7a
Boryana Dimitrova <b.dimitrova97@gmail.com>
2022-10-28 15:06:01 +0300

    Setup create functionality

--- src/controller/toysController.js
+++ src/controller/toysController.js
@@ -2,9 +2,23 @@ const router = require('express').Router();
 
 const toysServices = require('../services/toysServices');
 
-router.get('/catalog', (req, res) => {
-    const getToys = toysServices.getAll();
-    res.render('toys/catalog', { getToys });
+router.get('/catalog', async (req, res) => {
+    let toys = await toysServices.getAll();
+    res.render('toys/catalog', { toys });
+});
+
+router.get('/create-toy', (req, res) => {
+    res.render('toys/create');
+});
+
+router.post('/create-toy', async (req, res) => {
+    try {
+        await toysServices.create({ ...req.body, owner: req.user });
+        res.redirect('/toys/catalog');
+    } catch (error) {
+        console.log(error);
+        res.render('toys/create', error);
+    }
 })
 
 module.exports = router;
