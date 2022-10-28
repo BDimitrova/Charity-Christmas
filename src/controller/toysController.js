f9f6f0f1794d1fae491334a0497ae5bf5e73c5a9
Boryana Dimitrova <b.dimitrova97@gmail.com>
2022-10-28 16:10:11 +0300

    Setup Details page

--- src/controller/toysController.js
+++ src/controller/toysController.js
@@ -19,6 +19,19 @@ router.post('/create-toy', async (req, res) => {
         console.log(error);
         res.render('toys/create', error);
     }
+});
+
+router.get('/:toyId/details', async (req, res) => {
+    let toy = await toysServices.getOne(req.params.toyId);
+
+    let toyData = await toy.toObject();
+
+    let isOwner = toyData.owner == req.user?._id;
+    let buyer = toy.getBuying();
+    
+    let isBought = req.user && buyer.some(c => c._id == req.user?._id);
+
+    res.render('toys/details', { ...toyData, isOwner, isBought });
 })
 
 module.exports = router;
